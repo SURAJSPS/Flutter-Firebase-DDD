8d0eba8a5c405f806c31ec2fc97de43456eb1fdf
idreameducation <94044355+idreameducation@users.noreply.github.com>
2022-12-02 21:25:57 +0530

    ----

---
+++ lib/domain/core/value_object.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_firebase_ddd_with_bloc/domain/core/failure.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+   Either<ValueFailure<T>, T> get value;
+
+  @override
+  bool operator ==(covariant ValueObject other) {
+    if (identical(this, other)) return true;
+
+    return other.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'ValueObject(value: $value)';
+}
