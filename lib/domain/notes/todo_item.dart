4cdea51827b31daaaef2689d1d018e981ce70522
idreameducation <94044355+idreameducation@users.noreply.github.com>
2022-12-08 22:35:47 +0530

    Implement note validators and domain layer

---
+++ lib/domain/notes/todo_item.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_firebase_ddd_with_bloc/domain/core/value_objects.dart';
+import 'package:flutter_firebase_ddd_with_bloc/domain/notes/value_objects.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../core/failure.dart';
+part 'todo_item.freezed.dart';
+
+@freezed
+abstract class TodoItem implements _$TodoItem {
+  const TodoItem._();
+  const factory TodoItem({
+    required UniqueId id,
+    required TodoName name,
+    required bool done,
+  }) = _TodoItem;
+
+  factory TodoItem.empty() => TodoItem(
+        id: UniqueId(),
+        name: TodoName(""),
+        done: false,
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return name.value.fold((f) => some(f), (r) => none());
+  }
+}
