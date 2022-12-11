fe1433ec4515adabbc40a313be8b06103ffd8f1f
idreameducation <94044355+idreameducation@users.noreply.github.com>
2022-12-11 18:41:10 +0530

    Implement note repository with CRUD Operation

--- lib/application/notes/note_actor/note_actor_bloc.dart
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_firebase_ddd_with_bloc/domain/notes/i_note_repository.dart';
 import 'package:flutter_firebase_ddd_with_bloc/domain/notes/note.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 import '../../../domain/notes/note_failure.dart';
 
@@ -9,6 +10,7 @@ part 'note_actor_event.dart';
 part 'note_actor_state.dart';
 part 'note_actor_bloc.freezed.dart';
 
+@injectable
 class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
   NoteActorBloc(this._noteRepository) : super(const _Initial()) {
     on<_Deleted>((event, emit) => delete(event, emit));
